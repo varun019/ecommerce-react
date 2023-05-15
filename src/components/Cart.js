9ec869a21c2ec5a0c19900e5c2baaac1f3f96541
varun019 <82639067+varun019@users.noreply.github.com>
2023-05-15 19:27:22 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -27,7 +27,7 @@ const Cart = () => {
         </div>
         <hr />
         <div className="cart-item">
-          {cart.map((curElem) => {
+          {cart && cart.map((curElem) => {
             return <CartItem key={curElem.id} {...curElem} />;
           })}
         </div>
