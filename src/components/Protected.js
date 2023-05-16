552afb13e4573309b73db04e04f65f9f2a01feac
varun019 <82639067+varun019@users.noreply.github.com>
2023-05-16 19:36:19 +0530

    Update Protected.js

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Protected = (props) => {
     const navigate = useNavigate();
     const { Component } = props;
-    let login = JSON.parse(localStorage.getItem('register_user')|| "{}");
+    let login = JSON.parse(localStorage.getItem('register_user')|| "[]");
     useEffect(() => {
         const isLogged =  login.find((text)=> text.isLogin == true);
         if (!isLogged) {
